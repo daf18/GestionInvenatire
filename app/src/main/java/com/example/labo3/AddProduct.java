25268ce5c4a2d922539eff127f45752206d76325
Andreea <totyo_deea@yahoo.com>
2022-07-10 00:08:31 -0400

    add product form finished

---
+++ app/src/main/java/com/example/labo3/AddProduct.java
@@ -0,0 +1,109 @@
+package com.example.labo3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Parcelable;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class AddProduct extends AppCompatActivity {
+EditText etId, etName, etCateg, etPrice, etQuantity;
+Button btnCancel, btnAdd;
+boolean isAllFieldsChecked;
+Produit produit;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_product);
+
+        etId = findViewById(R.id.etId);
+        etName = findViewById(R.id.etName);
+        etCateg = findViewById(R.id.etCateg);
+        etPrice = findViewById(R.id.etPrice);
+        etQuantity = findViewById(R.id.etQuantity);
+
+        btnCancel = findViewById(R.id.btnCancel);
+        btnAdd = findViewById(R.id.btnAdd);
+
+        isAllFieldsChecked = false;
+
+btnCancel.setOnClickListener(view -> finish());
+
+btnAdd.setOnClickListener(view -> addProduct());
+
+    }
+    public void addProduct(){
+
+        isAllFieldsChecked = checkAllFields();
+        Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+        //envoyer le produit a MainActivity
+        intent.putExtra("produit", (Parcelable) produit);
+        startActivity(intent);
+        finish();
+
+    }
+    private boolean checkAllFields(){
+        String id = etId.getText().toString();
+        String name = etName.getText().toString();
+        String categ = etCateg.getText().toString();
+        String price = etPrice.getText().toString();
+        String qte = etQuantity.getText().toString();
+
+        if (etId.length() == 0) {
+            etId.setError("Identifient du produit requis!");
+            return false;
+        } else if(id.matches("/^\\d+$/")) {
+            etId.setError("Id contient des caractères invalides");
+            return false;
+        }
+
+        if (etName.length() == 0) {
+            etName.setError("Nom du produit requis!");
+            return false;
+        }else if(name.matches("/^\\w+/")) {
+            etName.setError("Nom contient des caractères invalides");
+            return false;
+        }
+
+        if (etCateg.length() == 0) {
+            etCateg.setError("Catégorie du produit requis!");
+            return false;
+        }else if(categ.matches("/^\\w+/")) {
+            etCateg.setError("Catégorie contient des caractères invalides");
+            return false;
+        }
+        if (etPrice.length() == 0) {
+            etPrice.setError("Prix du produit requis!");
+            return false;
+        }else if(price.matches("/^[0-9]+.[0-9]+$")) {
+            etPrice.setError("Prix contient des caractères invalides");
+            return false;
+        }
+
+        if (etQuantity.length() == 0) {
+            etQuantity.setError("Quantité du produit requis!");
+            return false;
+        }else if(qte.matches("/^\\d+$/")) {
+            etQuantity.setError("Quantité contient des caractères invalides");
+            return false;
+        }
+        Toast.makeText(this,"Produit "+id+" a été bien enregistré", Toast.LENGTH_LONG).show();
+        produit = new Produit(Integer.parseInt(id),
+                name,
+                categ,
+                Double.parseDouble(price),
+                Integer.parseInt(qte));
+        Log.i("produit",produit.toString());
+    // si tous les champs sont remplis , retourner true
+        return true;
+    }
+}
--- app/src/main/java/com/example/labo3/MainActivity.java
+++ app/src/main/java/com/example/labo3/MainActivity.java
@@ -2,42 +2,50 @@ package com.example.labo3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.nio.Buffer;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    TextView textView;
     ArrayList<Produit> listeProduits = new ArrayList<>(); //TODO check if it should be init here
     ArrayAdapter adapter;
     ListView listView;
+Button btnLister, btnAjouter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textView = findViewById(R.id.textView);
         listView = findViewById(R.id.listView);
-//        adapter = new ArrayAdapter<Produit>(this,
-//                android.R.layout.simple_list_item_1, listeProduits);
-//
+        btnLister = findViewById(R.id.btnLister);
+        btnAjouter = findViewById(R.id.btnAjouter);
+
           chargerProduits();
+
+          btnLister.setOnClickListener(view -> listerProduits());
+
+          btnAjouter.setOnClickListener(new View.OnClickListener() {
+              @Override
+              public void onClick(View view) {
+                  openAddProduct();
+              }
+          });
+        ajoutNouveauProduit();
     }
     public void chargerProduits(){
         String string = "";
-        StringBuilder stringBuilder = new StringBuilder();
         InputStream is = null;
         try {
             is = getAssets().open("produits");
@@ -59,29 +67,32 @@ public class MainActivity extends AppCompatActivity {
             String categ = parts[2];
             double prix = Double.parseDouble(parts[3]);
             int qte = Integer.parseInt(parts[4]);
+
             listeProduits.add(new Produit(id,nom,categ,prix,qte));
-            stringBuilder.append(listeProduits.toString());
-
-         //   textView.setText(stringBuilder); //TODO remove
-                    adapter = new ArrayAdapter<>(this,
-                android.R.layout.simple_list_item_1, listeProduits);
-            listView.setAdapter(adapter);
-
-//            for (String a : parts) {
-//                 System.out.println(a);
-//                stringBuilder.append(a).append("\n");
-//            }
-//              stringBuilder.append(string).append("\n");
-//
-//            textView.setText(stringBuilder);
         }
         try {
             is.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
-        Toast.makeText(getBaseContext(), stringBuilder.toString(),
-                Toast.LENGTH_LONG).show();
+    }
+    public void listerProduits(){
+        adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, listeProduits);
+        listView.setAdapter(adapter);
+    }
+
+    private void openAddProduct(){
+        Intent intent = new Intent(getApplicationContext(),AddProduct.class);
+        startActivity(intent);
+    }
+    public void ajoutNouveauProduit(){
+        Intent i = getIntent();
+        Produit nProduit = (Produit) i.getParcelableExtra("produit");
+        if(nProduit != null)
+        Log.i("produitMain",nProduit.toString());
+        listeProduits.add(nProduit);
+
     }
 }
 
