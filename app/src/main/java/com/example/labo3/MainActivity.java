53ca48c573982138fcd3b8295c89fa9edb9beac2
Andreea <totyo_deea@yahoo.com>
2022-07-11 22:05:31 -0400

    clened unused code

--- app/src/main/java/com/example/labo3/MainActivity.java
+++ app/src/main/java/com/example/labo3/MainActivity.java
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Environment;
 import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -12,20 +11,15 @@ import android.widget.ListView;
 import android.widget.Toast;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    ArrayList<Produit> listeProduits = new ArrayList<>(); //TODO check if it should be init here
+    //TODO check if it should be init here
+    ArrayList<Produit> listeProduits = new ArrayList<>();
     ArrayAdapter adapter;
     ListView listView;
     Button btnLister, btnAjouter,btnSave;
@@ -58,23 +52,6 @@ public class MainActivity extends AppCompatActivity {
 
 
     public void chargerProduits(){
-//        FileInputStream fis = null;
-//
-//        try {
-//            fis = openFileInput("produits.txt");
-//            InputStreamReader isr = new InputStreamReader(fis);
-//            BufferedReader br = new BufferedReader(isr);
-//            StringBuilder sb = new StringBuilder();
-//            String text;
-//
-//            while((text = br.readLine()) != null){
-//                sb.append(text).append("\n");
-//            }
-//        } catch (FileNotFoundException e) {
-//            e.printStackTrace();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
         String string = "";
         FileInputStream fis = null;
         try {
@@ -101,6 +78,7 @@ public class MainActivity extends AppCompatActivity {
             listeProduits.add(new Produit(id,nom,categ,prix,qte));
         }
         try {
+            assert fis != null;
             fis.close();
         } catch (IOException e) {
             e.printStackTrace();
@@ -108,27 +86,18 @@ public class MainActivity extends AppCompatActivity {
     }
     public void saveProducts() {
         ajoutNouveauProduit();
-//        try {
-//            BufferedWriter bw = new BufferedWriter(new FileWriter("assets/produits.txt"));
-//            for (Produit unProd : listeProduits) {
-//                bw.write(insert(unProd));
-//                bw.newLine();
-//            }
-//            bw.close();
-//        } catch (Exception ex) {
-//            ex.printStackTrace();
-//        }
-        String filecontent ="";
+
+        StringBuilder filecontent = new StringBuilder();
 
         for(Produit unProd : listeProduits) {
-            filecontent += insert(unProd);
+            filecontent.append(insert(unProd));
         }
-        Log.i("filecontent",filecontent);
+        Log.i("filecontent", filecontent.toString());
 
         FileOutputStream fos = null;
         try {
             fos = openFileOutput("produits.txt", MODE_PRIVATE);
-            fos.write(filecontent.getBytes());
+            fos.write(filecontent.toString().getBytes());
 
             Toast.makeText(this, "Saved to "+getFilesDir(),Toast.LENGTH_LONG).show();
 
@@ -143,7 +112,6 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         }
-
     }
 
     public void listerProduits(){
@@ -156,32 +124,15 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(getApplicationContext(),AddProduct.class);
         startActivity(intent);
     }
+
     public void ajoutNouveauProduit(){
         Intent i = getIntent();
-        Produit nProduit = (Produit) i.getParcelableExtra("produit");
+        Produit nProduit = i.getParcelableExtra("produit");
         if(nProduit != null)
         Log.i("produitMain",nProduit.toString());
         listeProduits.add(nProduit);
         Log.e("list",listeProduits.toString());
     }
-
-//        //TODO daca nu am prod nou si dau click it crashes
-//        for(Produit unProd : listeProduits){
-//            filecontent += insert(unProd);
-//        }
-//        if(!filecontent.equals("")){
-//            File myExternalFile = new File(getExternalFilesDir(filepath), filename);
-//            FileOutputStream fos = null;
-//            try {
-//                fos = new FileOutputStream(myExternalFile);
-//                fos.write(filecontent.getBytes());
-//            } catch (FileNotFoundException e) {
-//                e.printStackTrace();
-//            } catch (IOException e) {
-//                e.printStackTrace();
-//            }
-//            Toast.makeText(this, "Info Saved to file",Toast.LENGTH_SHORT).show();
-//        }
 
     //methode privée pour transformer le produit en string en ayant comme delimiteur ";"
     private String insert(Produit unProduit) {
